421f941f94c617c639dbdc704233e9ce04d00c1a
mmilton <murray.l.milton@outlook.com>
2021-07-16 06:43:37 +0200

    added footer enclosure to around paragraph tag to center copyright

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 function Footer() {
     const currentYear = new Date().getFullYear();
-    return <p>Copyright © {currentYear}</p>
+    return <footer>
+        <p>Copyright © {currentYear}</p>
+        </footer>
 }
 
 export default Footer;
